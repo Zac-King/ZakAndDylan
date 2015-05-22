a92dc59dfcd5aa3efd08580868677f419b9ea048
brokenCarry <thebrokekiller@yahoo.com>
2015-05-22 09:06:06 -0500

    added files
    took files from existing project on school PC to my main repository

---
+++ Assets/Scripts/Dylan/EnemyBehavior.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour 
+{
+    public GameObject m_target; //my target
+
+    private float m_attackRange = 1.5f;
+    private float m_moveSpeed = .005f;
+
+    private float m_move;
+
+    private int m_Strength = 1;
+
+    private bool targetDetected = false;
+    //private bool alert = false;
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if(c.gameObject.tag == "player")
+        {
+            m_target = c.gameObject;
+            targetDetected = true;
+            //alert = true;
+        }
+    }
+
+    private void attack()
+    {
+        float distance = Vector3.Distance(transform.position, m_target.transform.position);
+        if(distance <= m_attackRange)
+        {
+            m_target.GetComponent<PlayerStats>().m_currentHealth -= m_Strength;
+        }
+    }
+
+	void Start () {}
+	
+
+	void Update () 
+    {
+        if(targetDetected == true)
+        {
+            m_move = Time.deltaTime + m_moveSpeed;
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(m_target.transform.position - transform.position), Time.deltaTime * 5);
+            transform.position += transform.forward * m_move;
+            attack();
+        }
+	}
+}
---
+++ Assets/Scripts/Dylan/EnemySpawns.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawns : MonoBehaviour 
+{
+    public  GameObject[] enemy;
+    public int numSpawms = 3;
+    public bool spawn = false;
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "player")
+        {
+            spawn = true;  
+        }
+    }
+
+    private void OnTriggerExit(Collider c)
+    {
+        if (c.gameObject.tag == "player")
+        {
+            spawn = false;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (spawn == true)
+        {
+            for (int i = 1; i < numSpawms; i++)
+            {
+                if (i <= numSpawms)
+                {
+                    Vector3 push = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+
+                    switch (i % 3)
+                    {
+                        case 1: push.x += Random.Range(-3.0f, 3.0f); break;
+                        case 2: push.z += Random.Range(-3.0f, 3.0f); break;
+                    }
+
+                    push.Normalize();
+
+                    push *= Random.Range(.7f * i, i * 2.3f);
+
+
+                    Instantiate(enemy[i], transform.position + push, transform.rotation);
+                    print("I spawned");
+                }
+            }
+        }
+        //spawn = false;
+    }
+}
